220d8957fe21cf34b65f444f20f109b2ceda76b3
Integer003 <1204649252@qq.com>
2024-07-05 16:25:40 +0800

    add regulator basic

--- operator/src/main/scala/Global/ServiceCenter.scala
+++ operator/src/main/scala/Global/ServiceCenter.scala
@@ -9,21 +9,24 @@ object ServiceCenter {
   val projectName: String = "APP"
 
   val dbManagerServiceCode = "A000001"
-  val operatorServiceCode    = "A000002"
-  val sellerServiceCode   = "A000003"
+  val operatorServiceCode  = "A000002"
+  val sellerServiceCode    = "A000003"
   val portalServiceCode    = "A000004"
+  val regulatorServiceCode = "A000005"
 
   val fullNameMap: Map[String, String] = Map(
     dbManagerServiceCode ->  "数据库管理（DB_Manager）",
-    operatorServiceCode    ->  "运营方（Operator）",
-    sellerServiceCode   ->  "用户（Seller）",
-    portalServiceCode    ->  "门户（Portal）"
+    operatorServiceCode  ->  "运营方（Operator）",
+    sellerServiceCode    ->  "用户（Seller）",
+    portalServiceCode    ->  "门户（Portal）",
+    regulatorServiceCode ->  "监管方（Regulator）"
   )
 
   val address: Map[String, String] = Map(
     "DB-Manager" ->     "127.0.0.1:10001",
-    "Operator" ->         "127.0.0.1:10002",
-    "Seller" ->        "127.0.0.1:10003",
-    "Portal" ->         "127.0.0.1:10004"
+    "Operator"   ->     "127.0.0.1:10002",
+    "Seller"     ->     "127.0.0.1:10003",
+    "Portal"     ->     "127.0.0.1:10004",
+    "Regulator"  ->     "127.0.0.1:10005"
   )
 }
