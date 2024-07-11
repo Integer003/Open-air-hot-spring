f3c572bef3cffa53cb14db8d1b4afe8d891251a2
秦祉恺 <qinzk23@mails.tsinghua.edu.cn>
2024-07-11 23:20:29 +0800

    完善了货仓页面

--- seller/src/main/scala/Impl/SellerQueryStorageMessagePlanner.scala
+++ seller/src/main/scala/Impl/SellerQueryStorageMessagePlanner.scala
@@ -11,7 +11,7 @@ import io.circe.syntax.*
 import io.circe.{Decoder, Encoder, HCursor, Json}
 import cats.syntax.all.*
 
-case class StorageInfo(goodsID: String, goodsName: String, price: BigDecimal, description: String, condition: String, sellerName: String, buyerName: Option[String])
+case class StorageInfo(goodsID: String, goodsName: String, price: BigDecimal, description: String, condition: String, sellerName: String, buyerName: Option[String], verify: Option[String] = None, star: String)
 
 object StorageInfo {
   implicit val decoder: Decoder[StorageInfo] = new Decoder[StorageInfo] {
@@ -24,14 +24,16 @@ object StorageInfo {
         condition <- c.downField("condition").as[String]
         sellerName <- c.downField("sellerName").as[String]
         buyerName <- c.downField("buyerName").as[Option[String]]
-      } yield StorageInfo(goodsID, goodsName, price, description, condition, sellerName, buyerName)
+        verify <- c.downField("verify").as[Option[String]]
+        star <- c.downField("star").as[String].orElse(c.downField("star").as[Int].map(_.toString))
+      } yield StorageInfo(goodsID, goodsName, price, description, condition, sellerName, buyerName, verify, star)
   }
 }
 
 case class SellerQueryStorageMessagePlanner(userName: String, override val planContext: PlanContext) extends Planner[String]:
   override def plan(using planContext: PlanContext): IO[String] = {
     // 查询 goods_info 中 condition 为 'false' 且 seller_name 为当前用户的数据
-    val query = s"SELECT goods_id, goods_name, price, description, condition, seller_name, buyer_name FROM goods.goods_info WHERE condition = 'false' AND seller_name = ?"
+    val query = s"SELECT goods_id, goods_name, price, description, condition, seller_name, buyer_name, verify, star FROM goods.goods_info WHERE seller_name = ?"
 
     readDBRows(query, List(SqlParameter("String", userName))).flatMap { rows =>
       // 解码 rows 为 StorageInfo 然后转换为 JSON 字符串
